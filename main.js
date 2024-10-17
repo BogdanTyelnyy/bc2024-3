f4fcd5e7c5ace975f48ea902bf0212f3832cafa9
Bogdan Tyelnyy bc2024-3 <bogdan.tlny@gmail.com>
2024-10-17 03:09:55 +0300

    Options work

--- main.js
+++ main.js
@@ -2,15 +2,30 @@ const {program} = require('commander');
 const fs = require('node:fs');
 program
     .option('-i, --input <file path>', 'Takes file to read data')
-    .option('-o, --output <file path>', 'Write data into file')
-    .option('-d, --display', 'Outout data into console')
+    .option('-o, --output <file path>', 'Writes data into file')
+    .option('-d, --display', 'Outputs data into console')
     .action((opt) => {
         if(!opt.input) {
             console.error('Please, specify input file');
         } else if(!fs.existsSync(opt.input)) {
-            console.error('Cannot find the file');
+            console.error('Cannot find input file');
         } else {
-            
+            let data = fs.readFileSync(opt.input, {
+                encoding : 'utf-8'
+            });
+            let obj = JSON.parse(data), 
+                result = 0;
+            obj.forEach(el => {
+                result = el.rate > result ? el.rate : result;
+            });
+            if (opt.output) {
+                fs.writeFileSync(opt.output, 'Максимальний курс:' + result, {
+                    encoding : 'utf-8'
+                });
+            }
+            if (opt.display) {
+                console.log('Максимальний курс:' + result);
+            }
         }
     });
 
